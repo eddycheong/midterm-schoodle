317978b1b4b378be904259dff3cce3fdc66cc467
eddycheong <cheong.e49@gmail.com>
2017-10-28 22:46:43 +0000

    user can not hit submit and create new attendee

--- public/scripts/create-attendee.js
+++ public/scripts/create-attendee.js
@@ -1,36 +1,40 @@
 $(() => {
+  // function CreateNewName(name) {
 
+  //   return
 
-  function CreateNewName (name) {
+  //   $("button").click(function () {
+  //     $("p").prepend("name that I am grabing");
+  //   });
 
-    return
-
-      $("button").click(function(){
-      $("p").prepend("name that I am grabing");
-    });
-
-  };
-
-
+  // };
 
   function getNameAndRender() {
+    $('form').on('submit', function(event) {
+        event.preventDefault();
 
-
-    $('').on('submit', function (event) {
-
-
-    $.ajax({
-        method: 'POST',
-        url: '/routes', ??go to the server and render ?
-        success(attendee) {
-        renderRoutes(attendee);
-        }
+        const route = $(this).attr("action");
+        
+        $.ajax({
+          method: 'POST',
+          url: route,
+          data: $(this).serialize()
+        })
+        .done(response => {
+          console.log(response);
+        });
     });
+  }
 
-  });
+  // function getNameAndRender() {
+  //   $('').on('submit', function (event) {h
+  //     //   url: '/routes', ??go to the server and render ?
+  //     //     success(attendee) {
+  //     //       renderRoutes(attendee);
+  //     //     }
+  //     // });
+  //   });
 
-
-  getNameAndRender();
-
+    getNameAndRender();
 });
 
