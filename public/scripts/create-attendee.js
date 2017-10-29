8b0969e11330cb79e1fadb8a7765159d28ad7787
Mel <zzidante@gmail.com>
2017-10-29 23:30:12 +0000

    brought back edit button once submit is hit

--- public/scripts/create-attendee.js
+++ public/scripts/create-attendee.js
@@ -1,5 +1,9 @@
 $(() => {
 
+  // Hit Edit Button
+      $('#editBtn').on('click', function() {
+      })
+
   function buildAttendeeResponses(array) {
     return {
       attendeeName: array[0],
@@ -22,15 +26,9 @@ $(() => {
       })
         .done(() => {
 
-      // Hit Submit Button
-          $('#attendee-form-submit-button').one('click', function() {
-            $('<input id="editBtn" class="btn btn-primary btn-lg"></input>').attr({'type': 'button'}).val("Edit Your Entry").click(function(){
-            }).appendTo($('th.proposal-display-table-headers'));
-          });
-
-      // Hit Edit Button
-          $('#editBtn').on('click', function() {
-          })
+      // Make Edit Button
+      $('<input id="editBtn" class="btn btn-primary btn-lg"></input>').attr({'type': 'button'}).val("Edit Your Entry").click(function(){
+      }).appendTo($('th.proposal-display-table-headers'));
 
       // Add new Row With Attendee
           $("#attendee-input")
@@ -74,7 +72,6 @@ $(() => {
             );
           }
 
-
           const row = $("<tr>").addClass("display-table-row")
             .append(name)
 
@@ -91,6 +88,80 @@ $(() => {
     });
   }
   
+
+
+//
+//
+  // $.ajax({
+  //   method: 'PUT',
+  //   url: route,   // right route
+  //   data: buildAttendeeResponses(newAttendee.serializeArray())   // ?
+  // })
+  //   .done(() => {
+
+  // // Show new ROW
+
+  // $('<input id="editBtn" class="btn btn-primary btn-lg"></input>').attr({'type': 'button'}).val("Edit Your Entry").click(function(){
+  // }).appendTo($('th.proposal-display-table-headers'));
+
+  // // Add new Row With Attendee
+  //     $("#attendee-input")
+  //       .closest('.display-table-row')
+  //       .css('display', 'none');
+
+  //     const newRow = {};
+
+  //     newAttendee.serializeArray().forEach(elem => {
+  //       if (elem.name === "attendeeName") {
+  //         newRow["attendeeName"] = elem.value;
+  //       }
+  //     });
+
+  //     const name = $("<td>")
+  //       .addClass("proposal-display-table-attendee-name")
+  //       .text(newRow.attendeeName);
+
+  //     const attendeeFullResponses = newAttendee.serializeArray();
+  //     const attendeeCheckboxAnswers = attendeeFullResponses.slice(2, attendeeFullResponses.length)
+
+  //     const yesDateOptions = (attendeeCheckboxAnswers
+  //       ? attendeeCheckboxAnswers.reduce((obj, item) => (obj[item.name] = item.value, obj), {})
+  //       : {});
+
+  //     console.log(attendeeCheckboxAnswers);
+  //     console.log("yes date: ", yesDateOptions);
+
+
+  //     const response = (answer) => {
+  //       if(answer) {
+  //         return $("<td>")
+  //         .append(
+  //           $("<i>").addClass("fa fa-check")
+  //         );  
+  //       }
+
+  //       return $("<td>")
+  //       .append(
+  //         $("<i>").addClass("fa fa-times")
+  //       );
+  //     }
+
+  //     const row = $("<tr>").addClass("display-table-row")
+  //       .append(name)
+
+  //     const eventRows = $('thead > tr').children();
+  //     const eventDateOptions = eventRows.slice(1, eventRows.length);
+
+  //     eventDateOptions.each(function(index, elem) {
+  //       const answer = yesDateOptions[$(elem).data('id')];
+  //       row.append(response(!!answer))
+  //     });
+
+  //     $("tbody").append(row);
+  //   });
+//
+//
+
   getNameAndRender();
 });
 
